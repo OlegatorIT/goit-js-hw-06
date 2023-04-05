5ff49092926a2a1d2fa25e47e05dfd0160dc1163
OlegatorIT <podlesnyoleg93@gmail.com>
2023-04-05 18:26:11 +0200

    task 'One' done

---
+++ js/task-01.js
@@ -0,0 +1,24 @@
+const numCateroties = document.querySelectorAll("#categories .item");
+console.log(`Number of categories: ${numCateroties.length}`);
+numCateroties.forEach((el, index) =>
+  console.log(`
+  Category: ${el.querySelector("h2").textContent}
+  Elements: ${el.querySelectorAll("ul li").length}
+  `)
+);
+
+// const idCategories = document.querySelector("#categories");
+// const allCategories = idCategories.querySelectorAll(".item").length;
+// const category = idCategories.querySelectorAll("h2");
+// const ulCategory = idCategories.querySelectorAll(".item ul ");
+
+// console.log(`Number of categories: ${Number(allCategories)}
+
+// Category: ${category[0].textContent}
+// Elements: ${ulCategory[0].querySelectorAll("li").length}
+
+// Category: ${category[1].textContent}
+// Elements: ${ulCategory[1].querySelectorAll("li").length}
+
+// Category: ${category[2].textContent}
+// Elements: ${ulCategory[2].querySelectorAll("li").length}`);
