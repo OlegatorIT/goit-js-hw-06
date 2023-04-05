2c215794b9bbae77ff2ce82d3347e1be77bfb9b2
OlegatorIT <podlesnyoleg93@gmail.com>
2023-04-05 23:36:15 +0200

    task 2 , done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+function createEl(value) {
+  const list = document.querySelector("#ingredients");
+  value.map((el) => {
+    const createLi = document.createElement("li");
+    createLi.classList.add("item");
+    createLi.textContent = `${el}`;
+    list.append(createLi);
+  });
+  return list;
+}
+
+console.log(createEl(ingredients));
