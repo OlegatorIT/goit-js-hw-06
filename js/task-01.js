5f7ad0df8bd763b93e71f85fbcd32fa99c8fbe43
OlegatorIT <podlesnyoleg93@gmail.com>
2023-04-05 18:30:05 +0200

    delete excess

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,6 @@
 const numCateroties = document.querySelectorAll("#categories .item");
 console.log(`Number of categories: ${numCateroties.length}`);
-numCateroties.forEach((el, index) =>
+numCateroties.forEach((el) =>
   console.log(`
   Category: ${el.querySelector("h2").textContent}
   Elements: ${el.querySelectorAll("ul li").length}
