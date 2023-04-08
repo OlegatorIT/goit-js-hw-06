bbdffe0b02ef70f87242171894d68ad8d89383cc
OlegatorIT <podlesnyoleg93@gmail.com>
2023-04-09 01:26:04 +0200

    task 8 is done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const loginFormEl = document.querySelector(".login-form");
 
+loginFormEl.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Будьласка заповніть всі поля !");
+  }
+
+  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
