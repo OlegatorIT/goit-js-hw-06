4e04a1e9e15335dceed01b221305d5dc5750a256
OlegatorIT <podlesnyoleg93@gmail.com>
2023-04-09 02:05:52 +0200

    task 9 is done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyColor = document.body;
+const spanColor = document.querySelector(".color");
+const btnColor = document.querySelector(".change-color");
+
+btnColor.addEventListener("click", () => {
+  bodyColor.style.backgroundColor = getRandomHexColor();
+  console.log((spanColor.textContent = bodyColor.style.backgroundColor));
+  console.log(bodyColor.style.backgroundColor);
+});
